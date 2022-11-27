c806af877fca30c789660ba727afa60e2fb7dbc6
grospector <pitchakorngraph@gmail.com>
2022-11-27 18:02:54 +0700

    Update style dashboard

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestingComponent implements OnInit {
 
+  containerModule = document.querySelector<HTMLElement>('.parent');
+  mouseDown = false;
+  startX: any;
+  scrollLeft: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  startDragging(e:any, flag:any, el:any) {
+    this.mouseDown = true;
+    this.startX = e.pageX - el.offsetLeft;
+    this.scrollLeft = el.scrollLeft;
+  }
+  stopDragging(e:any, flag:any) {
+    this.mouseDown = false;
+  }
+  moveEvent(e:any, el:any) {
+    e.preventDefault();
+    if (!this.mouseDown) {
+      return;
+    }
+    console.log(e);
+    const x = e.pageX - el.offsetLeft;
+    const scroll = x - this.startX;
+    el.scrollLeft = this.scrollLeft - scroll;
+  }
 }
