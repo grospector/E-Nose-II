5dba3aa3a2732ebebc518fc6d54700c0ca4fdbe1
grospector <pitchakorngraph@gmail.com>
2023-05-11 23:58:38 +0700

    Update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.enosecovid19.com",
+  apiUrl: "https://api-enose.at44.co.th",
   socketUrl: "https://socket2.at44.co.th",
   webUrl: "https://enose.covid19.com"
 };
